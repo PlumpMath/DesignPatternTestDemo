c8a57b435bc0cd6110609d5ad0df82156581c542
UglyMelon007 <UglyMelon007@gmail.com>
2016-06-03 17:29:27 +0800

    add AdapterPattern,TemplateMethodPattern,IteratorPattern and FacadePattern

---
+++ UM007.DesignPatternTest/UM007.DesignPatternTest.AdapterPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UM007.DesignPatternTest.AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDog dog = new WildDog();
+            IDuck duck = new WildDuck();
+            dog.Bark();
+            duck.Quack();
+            dog.Jump();
+            duck.Fly();
+            DuckAdapter adapterDuck = new DuckAdapter(dog);
+            adapterDuck.Quack();
+            adapterDuck.Fly();
+        }
+    }
+}
